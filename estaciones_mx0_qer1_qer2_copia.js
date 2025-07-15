6a27564ba572f2c34656db8835e3995e7fc87ee0
librekcesar <140665209+librekcesar@users.noreply.github.com>
2025-07-15 14:51:29 -0600

    Add files via upload

---
+++ estaciones_mx0_qer1_qer2_copia.js
@@ -0,0 +1,10 @@
+var estaciones = {
+"type": "FeatureCollection",
+"name": "estaciones_mx0_qer1_qer2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "QER1", "description": "QER1", "t_estacion": "Origen" }, "geometry": { "type": "Point", "coordinates": [ -100.3729844, 20.5719875, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "QER2", "description": "QER2", "t_estacion": "Origen" }, "geometry": { "type": "Point", "coordinates": [ -100.4535469, 20.5403875, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "19.64220097106514, -99.14250999381147", "description": "MX0", "t_estacion": "Origen" }, "geometry": { "type": "Point", "coordinates": [ -99.1422607, 19.6422227, 0.0 ] } }
+]
+}
---
+++ paradas_mx0_qer1_qer2_copia.js
@@ -0,0 +1,9 @@
+var seguras = {
+"type": "FeatureCollection",
+"name": "paradas_mx0_qer1_qer2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Name": "km 156.3, Autop. México - Querétaro, Palmillas, 76830 San Juan del Río, Qro.", "description": "PEMEX", "direccion": "km 156.3, Autop. México - Querétaro, Palmillas, 76830 San Juan del Río, Qro." }, "geometry": { "type": "Point", "coordinates": [ -99.9323535, 20.3149326, 0.0 ] } },
+{ "type": "Feature", "properties": { "Name": "Carrt. Nal. Mexico-Queretaro Km 48, 54770 Teoloyucan, Méx.\n+52 800 736 3900", "description": "PEMEX", "direccion": "Carrt. Nal. Mexico-Queretaro Km 48, 54770 Teoloyucan, Méx.\n+52 800 736 3900" }, "geometry": { "type": "Point", "coordinates": [ -99.2144551, 19.7600034, 0.0 ] } }
+]
+}
